b3f5fdeaac6547025b9bda72b8c10f0ec15f28aa
ludwig234 <ludwig.liljegren@gmail.com>
2023-03-05 14:55:33 +0100

    Update run.sh
    No more plymouth

--- setup/run.sh
+++ setup/run.sh
@@ -15,5 +15,4 @@ SETUP_SCRIPTS_DIR=$MAGIC_MIRROR_RASP_LITE_DIR/setup/scripts
 . $SETUP_SCRIPTS_DIR/copy-app-files.sh
 . $SETUP_SCRIPTS_DIR/setup-boot-options.sh
 . $SETUP_SCRIPTS_DIR/setup-magic-mirror-service.sh
-. $SETUP_SCRIPTS_DIR/setup-plymouth.sh
 . $SETUP_SCRIPTS_DIR/setup-xserver-on-startup.sh
