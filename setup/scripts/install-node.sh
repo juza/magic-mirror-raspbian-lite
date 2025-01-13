d132150a4434c5ee88e107857128a4bff278cdc9
juza <jurgen@zid24.se>
2025-01-13 14:38:49 +0100

    Update install-node.sh

--- setup/scripts/install-node.sh
+++ setup/scripts/install-node.sh
@@ -9,7 +9,7 @@ else
 
     cd ~/
 
-    NODE_MAJOR_VERSION="16"
+    NODE_MAJOR_VERSION="22"
     curl -sL https://deb.nodesource.com/setup_$NODE_MAJOR_VERSION.x | sudo -E bash -
     sudo apt install -y nodejs
 
